cf19b1d60517b8218136dd3053652efeda8808a7
Peter <wingsaseagles12@gmail.com>
2026-06-25 13:46:44 -0600

    Co-authored-by: lukecc25 <lukecc25@users.noreply.github.com> Co-authored-by: Josh <joshuazob@gmail.com> Co-authored-by: hyrum james bullock <HyrumB@users.noreply.github.com>

---
+++ client/app.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Header from './Header.svelte';
+    import HeroSection from './HeroSection.svelte';
+    import Footer from './Footer.svelte';
+
+    let description = $state("Come one come all! and learn about to the world of cooking with WebChef. The one stop shop for all your culinary needsfrom easy to hard lessons about ingreadients and cooking methods. Whether you are a beginner or a seasoned pro, WebChef has got you covered. Take da lessons or submit your own. We hope this site helps the cooking communtity prosper.");
+
+    function handleHeroAction() {
+        console.log("Action received from HeroSection");
+    }
+</script>
+
+<Header />
+
+<main class="main">
+    <HeroSection 
+        heroSrc="/images/hero.jpg" 
+        {description} 
+        onAction={handleHeroAction} 
+    />
+</main>
+
+<Footer />
