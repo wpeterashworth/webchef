92406d3093e315065b1ab149420c5cce865a980e
Josh <joshuazob@gmail.com>
2026-06-30 13:54:49 -0600

    started styling

--- client/components/footer.svelte
+++ client/components/footer.svelte
@@ -1,3 +1,11 @@
     <footer>
       <p>&copy; 2026 WebChef. All rights reserved.</p>
     </footer>
+
+    <style>
+      footer {
+        background-color: #f8f8f8;
+        padding: 20px;
+        text-align: center;
+      }
+    </style>
