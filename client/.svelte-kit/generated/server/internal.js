bba61551aac6b89ad00fb790f423490c07a27242
Josh <joshuazob@gmail.com>
2026-07-09 13:29:11 -0600

    updates

--- client/.svelte-kit/generated/server/internal.js
+++ client/.svelte-kit/generated/server/internal.js
@@ -4,6 +4,7 @@ import { set_building, set_prerendering } from '$app/env/internal';
 import { set_assets } from '$app/paths/internal/server';
 import { set_manifest, set_read_implementation } from '__sveltekit/server';
 import { set_private_env, set_public_env } from '../../../node_modules/.pnpm/@sveltejs+kit@2.68.0_@svelt_4f9ae6c1a53b45e493a0bedcde37f9fc/node_modules/@sveltejs/kit/src/runtime/shared-server.js';
+import { set_private_env, set_public_env } from '../../../node_modules/.pnpm/@sveltejs+kit@2.68.0_@svelt_4f9ae6c1a53b45e493a0bedcde37f9fc/node_modules/@sveltejs/kit/src/runtime/shared-server.js';
 import error from '../shared/error-template.js';
 
 export const options = {
@@ -23,7 +24,7 @@ export const options = {
 	service_worker_options: undefined,
 	server_error_boundaries: false,
 	templates: {
-		app: ({ head, body, assets, nonce, env }) => "<!doctype html>\r\n<html lang=\"en\">\r\n  <head>\r\n    <meta charset=\"utf-8\" />\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\r\n    " + head + "\r\n  </head>\r\n  <body data-sveltekit-preload-data=\"hover\">\r\n    <div style=\"display: contents\">" + body + "</div>\r\n  </body>\r\n</html>\r\n",
+		app: ({ head, body, assets, nonce, env }) => "<!doctype html>\r\r\n<html lang=\"en\">\r\r\n  <head>\r\r\n    <meta charset=\"utf-8\" />\r\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\r\r\n    " + head + "\r\r\n  </head>\r\r\n  <body data-sveltekit-preload-data=\"hover\">\r\r\n    <div style=\"display: contents\">" + body + "</div>\r\r\n  </body>\r\r\n</html>\r\r\n",
 		error
 	},
 	version_hash: "1g47t6h"
