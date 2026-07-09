9367fc341e6146b22fe86baa2cb65931034a772c
Josh <joshuazob@gmail.com>
2026-07-09 13:30:00 -0600

    not sure what changed

--- client/.svelte-kit/ambient.d.ts
+++ client/.svelte-kit/ambient.d.ts
@@ -158,7 +158,8 @@ declare module '$env/static/private' {
  * The above values will be the same _even if_ different values for `ENVIRONMENT` or `PUBLIC_BASE_URL` are set at runtime, as they are statically replaced in your code with their build time values.
  */
 declare module '$env/static/public' {
-	
+	export const PUBLIC_SUPABASE_ANON_KEY: string;
+	export const PUBLIC_SUPABASE_URL: string;
 }
 
 /**
@@ -347,6 +348,8 @@ declare module '$env/dynamic/private' {
  */
 declare module '$env/dynamic/public' {
 	export const env: {
+		PUBLIC_SUPABASE_ANON_KEY: string;
+		PUBLIC_SUPABASE_URL: string;
 		[key: `PUBLIC_${string}`]: string | undefined;
 	}
 }
